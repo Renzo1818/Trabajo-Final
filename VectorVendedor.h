1e3d1c5b68626834744c97bc560d845b3072e971
Renzo1818 <93232895+Renzo1818@users.noreply.github.com>
2022-05-31 16:28:22 -0500

    Update VectorVendedor.h

--- VectorVendedor.h
+++ VectorVendedor.h
@@ -1,55 +1,69 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-#include"ClaseVendedor.h"
+#include"Vendedor.h"+
 using namespace std;
 
 class VendedorVector
 {
-  private: 
-  vector<Vendedor> vectorVendedor;
+private:
+	vector<Vendedor> vectorVendedor;
 
-  public:
-  VendedorVector()
-  {
-    vector<Vendedor> vectorVendedor;
-  }
+public:
+	VendedorVector()
+	{
+		vector<Vendedor> vectorVendedor;
+	}
 
-  int getCorrelativoCodigo() 
-    {
-      if(vectorVendedor.size()==0){
-        return 1;
-      }
-      else
-      {
-        return vectorVendedor[vectorVendedor.size()-1].getCodigo()+1;
-      }
-    }
+	void agregar(Vendedor obj)
+	{
+		vectorVendedor.push_back(obj);
+	}
 
+	int rows() { //Cantidad de filas
+		return vectorVendedor.size();
+	}
 
-  void agregar(Vendedor obj)
-		{
-			vectorVendedor.push_back(obj);
-		}
-
-
-  void grabarEnArchivoVendedor(Vendedor v)
-		{
-			try
-			{
-				fstream archivoVendedor;
-				archivoVendedor.open("archivoVendedores.csv",ios::app);
-        
-				if(archivoVendedor.is_open())
-				{
-					archivoVendedor<<v.getCodigo()<<";"<<v.getUsuario()<<";"<<v.getContrasena()<<endl;
-					archivoVendedor.close();
-				}
-			}			
-			catch(exception e)
-			{
-				cout<<"Error";
+	int getPosArray(Vendedor obj) {
+		for (int i = 0; i < rows(); i++) {
+			if (obj.getContrasena() == vectorVendedor[i].getContrasena()) {
+				return i;
 			}
 		}
+		return -1;
+	}
+	Vendedor get(int pos) { //Entregar el objeto actual, segun la posicion pasada como parametro
+		return vectorVendedor[pos];
+	}
+
+	int getCorrelativoCodigo()
+	{
+		if (vectorVendedor.size() == 0) {
+			return 1;
+		}
+		else
+		{
+			return vectorVendedor[vectorVendedor.size() - 1].getCodigo() + 1;
+		}
+	}
+
+	void grabarEnArchivoVendedor(Vendedor v)
+	{
+		try
+		{
+			fstream archivoVendedor;
+			archivoVendedor.open("archivoVendedores.csv", ios::app);
+
+			if (archivoVendedor.is_open())
+			{
+				archivoVendedor << v.getCodigo() << ";" << v.getUsuario() << ";" << v.getContrasena() << endl;
+				archivoVendedor.close();
+			}
+		}
+		catch (exception e)
+		{
+			cout << "Error";
+		}
+	}
 
 };
