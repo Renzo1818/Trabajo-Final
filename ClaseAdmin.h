b2aec97f257942d82b5eadd2f7fe8395d334464a
Renzo1818 <93232895+Renzo1818@users.noreply.github.com>
2022-06-15 15:15:46 -0500

    Update ClaseAdmin.h

--- ClaseAdmin.h
+++ ClaseAdmin.h
@@ -3,42 +3,42 @@
 using namespace std;
 class Admin
 {
-  private:
+private:
     int idCodigo;
     string usuario;
     int contrasena;
-  public:
+public:
 
-  Admin()
-  {
-    
-  }
+    Admin()
+    {
 
-//sets
-void setIdCodigo(int valor)
-{
-  idCodigo = valor;  
-}
-void setUsuario(string valor)
-{
-  usuario = valor;
-}
-void setContrasena(int valor)
-{
-  contrasena = valor;
-}
-//Get
-int getIdCodigo()
-{
-  return idCodigo;
-}
-string getUsuario()
-{
-  return usuario;
-}
-int getContrasena()
-{
-  return contrasena;
-}
+    }
+
+    //sets
+    void setIdCodigo(int valor)
+    {
+        idCodigo = valor;
+    }
+    void setUsuario(string valor)
+    {
+        usuario = valor;
+    }
+    void setContrasena(int valor)
+    {
+        contrasena = valor;
+    }
+    //Get
+    int getIdCodigo()
+    {
+        return idCodigo;
+    }
+    string getUsuario()
+    {
+        return usuario;
+    }
+    int getContrasena()
+    {
+        return contrasena;
+    }
 
 };
