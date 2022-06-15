cb3914c6396c9ad712a4dfc76902a2b1ef507745
Renzo1818 <93232895+Renzo1818@users.noreply.github.com>
2022-06-15 15:15:13 -0500

    Update RegistroAdmin.h

--- RegistroAdmin.h
+++ RegistroAdmin.h
@@ -4,114 +4,28 @@
 #include <fstream>
 #include <string>
 #include <cstdlib>
-#include "ClaseAdmin.h"
+#include "Admin.h"
 using namespace std;
 
 class AdminVector
 {
-  private:
-  vector<Admin> vectorAdmin;
+private:
+	vector<Admin> vectorAdmin;
 
-  public:
-  AdminVector()
-  {
-    vector<Admin> vectorAdmin;
-    cargarDatosDelArchivoAlVector();
-    
-  };
-
-void add(Admin obj)
-{
-  vectorAdmin.push_back(obj);
-}
-
-int getCorrelativoCodigo()
+public:
+	AdminVector()
 	{
-		if (vectorAdmin.size() == 0) {
-			return 1;
-		}
-		else
-		{
-			return vectorAdmin[vectorAdmin.size() - 1].getIdCodigo() + 1;
-		}
-	}
+		vector<Admin> vectorAdmin;
+		cargarDatosDelArchivoAlVector();
+		
 
-void grabarEnArchivoAdmin(Admin a)
-{
-	try
+	};
+
+	void add(Admin obj)
 	{
-		fstream archivoAdmin;
-		archivoAdmin.open("archivoAdmin.txt", ios::app);
-
-		if (archivoAdmin.is_open())
-		{
-			archivoAdmin << a.getIdCodigo() << ";" << a.getUsuario() << ";" << a.getContrasena() << ";" << endl;
-			archivoAdmin.close();
-		}
+		vectorAdmin.push_back(obj);
 	}
-	catch (exception e)
-	{
-		cout << "Error";
-	}
-}
-
-void cargarDatosDelArchivoAlVector() 
-{
-	try 
-  {
-		int i;
-		string linea;
-		size_t posi;
-		string temporal[3];
-		fstream archivoAdmin;
-      
-		archivoAdmin.open("archivoAdmin.txt", ios::in);
-		if (archivoAdmin.is_open())
-		{
-			while (!archivoAdmin.eof()) 
-      {
-				while (getline(archivoAdmin,linea)) 
-        {
-					i = 0;
-					while ((posi = linea.find(";")) != string::npos) 
-          { 
-						temporal[i] = linea.substr(0, posi);
-						linea.erase(0, posi + 1);	
-						i++;
-					}
-					
-				  Admin objAdmin;
-				  objAdmin.setIdCodigo(std::stoi(temporal[0]));
-					objAdmin.setUsuario(temporal[1]);
-					objAdmin.setContrasena(temporal[2]);
-
-				add(objAdmin);
-				}
-			}
-		}
-		archivoAdmin.close();
-	}
-	catch(exception e) 
-  {
-		cout << "Ocurrio un problema al momento de leer el archivo!!!";
-	}
-}
-
-bool ValidacionArchivo()
-{
-  fstream archivoAdmin;
-  archivoAdmin.open("archivoAdmin.txt", ios::in);
-  if (archivoAdmin.eof())
-  {
-   return true;
-  }
-  else 
-  {
-   return false;
-  }
-}
-
-bool getValidacionAdmin(Admin obj)
+	bool getValidacionAdmin(Admin obj)
 	{
 		for (int i = 0; i < vectorAdmin.size(); i++)
 		{
@@ -123,5 +37,128 @@ bool getValidacionAdmin(Admin obj)
 		}
 		return false;
 	}
+
+	int getCorrelativoCodigo()
+	{
+		if (vectorAdmin.size() == 0) {
+			return 1;
+		}
+		else
+		{
+			return vectorAdmin[vectorAdmin.size() - 1].getIdCodigo() + 1;
+		}
+	}
+
+	void grabarEnArchivoAdmin(Admin a)
+	{
+		try
+		{
+			fstream archivoAdmin;
+			archivoAdmin.open("archivoAdmin.txt", ios::app);
+
+			if (archivoAdmin.is_open())
+			{
+				archivoAdmin << a.getIdCodigo() << ";" << a.getUsuario() << ";" << a.getContrasena() << ";" << endl;
+				archivoAdmin.close();
+			}
+		}
+		catch (exception e)
+		{
+			cout << "Error";
+		}
+	}
+
+	//Validar si el vector esta vacio
+	void cargarDatosDelArchivoAlVector()
+	{
+		try
+		{
+			int i;
+			string linea;
+			size_t posi;
+			string temporal[3];
+			fstream archivoAdmin;
+
+			archivoAdmin.open("archivoAdmin.txt", ios::in);
+			if (archivoAdmin.is_open())
+			{
+				while (!archivoAdmin.eof())
+				{
+					while (getline(archivoAdmin, linea))
+					{
+						i = 0;
+						while ((posi = linea.find(";")) != string::npos)
+						{
+							temporal[i] = linea.substr(0, posi);
+							linea.erase(0, posi + 1);
+							i++;
+						}
+
+						Admin objAdmin;
+						objAdmin.setIdCodigo(std::stoi(temporal[0]));
+						objAdmin.setUsuario(temporal[1]);
+						objAdmin.setContrasena(std::stoi(temporal[2]));
+
+						add(objAdmin);
+					}
+				}
+			}
+			archivoAdmin.close();
+		}
+		catch (exception e)
+		{
+			cout << "Ocurrio un problema al momento de leer el archivo!!!";
+		}
+	}
+
+	bool validacionArchivoVacio()
+	{
+			ifstream archivoAdmin;
+			archivoAdmin.open("archivoAdmin.txt", ios::in);
+			if (archivoAdmin.is_open()) {
+				while(!archivoAdmin.eof())
+				{
+					if (sizeof(archivoAdmin) == 0) {
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				}
+			}
+			archivoAdmin.close();
+		
+	}
+
+
+
+
+
+
+
+
+
+	/*bool validacionArchivoVacio()
+	{
+		try {
+			ifstream archivoAdmin;
+			archivoAdmin.open("archivoAdmin.txt", ios::in);
+			if (archivoAdmin.is_open()) {
+				if (archivoAdmin.eof())
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			archivoAdmin.close();
+		}
+		catch (exception e) {
+			return 
+		}
+	}*/
 
 };
