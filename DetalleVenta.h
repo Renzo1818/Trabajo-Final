7448edf4439efc029ddcc21616df47362124cdd9
Renzo1818 <93232895+Renzo1818@users.noreply.github.com>
2022-06-21 19:36:59 -0500

    Update DetalleVenta.h

--- DetalleVenta.h
+++ DetalleVenta.h
@@ -1,20 +1,40 @@
 #pragma once
 #include<iostream>
-#include "Venta.h"
-
+#include "Producto.h"
 using namespace std;
 
-class DetalleVenta : public Venta {
+class DetalleVenta: public Producto{
 private:
-    int codVenta;//Hacer sobrecarga
-    int codProducto; //Hacer sobrecarga
+    int codVenta;
     int cantidad;
-    float precioVenta;
-public:
-    /*DetalleVenta() : Venta() {
 
-    }*/
+public:
+    DetalleVenta(int cod, string nom, float precio_, int stock_, int codVenta_, int cantidad_): 
+        Producto(cod, nom, precio_, stock_) {
+       codVenta = codVenta_;
+       cantidad = cantidad_;
+
+    }
+    DetalleVenta() {
+
+    }
+    //Set
+    void setCodVenta(int valor) {
+        codVenta = valor;
+    }
+    void setCantidad(int valor) {
+        cantidad = valor;
+    }
+    //Get
+    int getCodVenta() {
+        return codVenta;
+    }
+    int getCantidad() {
+        return cantidad;
+    }
+    //Metodos
+
     float getImporte() {
-        return cantidad * precioVenta;
+        return getCantidad() * getPrecio();
     }
 };
